14125ac2d954841abc8fbd4a51af2629e584bd6f
kaanyagci <kaan.yagci@clique.paris>
2021-10-26 12:16:15 +0200

    feat(selectors): selectors updated
    user profile base selectors added

--- src/selectors.ts
+++ src/selectors.ts
@@ -4,4 +4,18 @@ export default {
     password: '//*[@id="session_password"]',
     submit: '//button[contains(@class,"sign-in-form__submit-button")]',
   },
+  user: {
+    profile: {
+      base: {
+        fullName: "//div[contains(@class,'pv-text-details__left-panel')][1]/div[1]/h1",
+        shortDesc: "//div[contains(@class,'pv-text-details__left-panel')][1]/div[2]",
+        location: "//div[contains(@class,'pv-text-details__left-panel')][2]/span[1]",
+        contactInfoButton: "//div[contains(@class,'pv-text-details__left-panel')][2]/span[2]",
+        info: "//section[contains(@class,'pv-about-section')]/div",
+        infoTextToDelete: "//section[contains(@class,'pv-about-section')]/div/span",
+        premiumBadge: "//li-icon[@type='linkedin-bug']",
+        influencerBadge: "//li-icon[@type='linkedin-influencer-color-icon']",
+      },
+    },
+  },
 };
