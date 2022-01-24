7088f13531e760ccd9e1088f06faced9dd296f11
kaanyagci <kaan.yagci@clique.paris>
2022-01-24 22:45:14 +0100

    feat(cli): logout command implemented in linked topic

--- cli/src/commands/linked/logout.ts
+++ cli/src/commands/linked/logout.ts
@@ -1,28 +1,32 @@
 import { Command, Flags } from '@oclif/core';
+import { cli } from 'cli-ux';
+import { unlinkSync } from 'fs';
+import ConfigHandler from '../../commons/config-handler';
+import { Services } from '../../commons/services';
 
 export default class LinkedLogout extends Command {
-  static description = 'describe the command here';
+  private readonly configHandler: ConfigHandler = new ConfigHandler(Services.Linked, this.config.name);
+
+  static description = 'remove the saved LinkedIn account';
 
   static examples = ['<%= config.bin %> <%= command.id %>'];
 
   static flags = {
-    // flag with a value (-n, --name=VALUE)
-    name: Flags.string({ char: 'n', description: 'name to print' }),
-    // flag with no value (-f, --force)
-    force: Flags.boolean({ char: 'f' })
+    username: Flags.string({ char: 'u', description: 'username of the account to remove', required: false })
   };
 
-  static args = [{ name: 'file' }];
-
   public async run(): Promise<void> {
-    const { args, flags } = await this.parse(LinkedLogout);
-
-    const name = flags.name ?? 'world';
-    this.log(
-      `hello ${name} from /Users/kaanyagci/Documents/makepad/scraping.house/cli/src/commands/linked/logout.ts`
-    );
-    if (args.file && flags.force) {
-      this.log(`you input --force and --file: ${args.file}`);
+    const { flags } = await this.parse(LinkedLogout);
+    if (flags.username === undefined) {
+      flags.username = await cli.prompt("What's the username of the account to remove ?", { type: 'normal' });
     }
+    const confirmed: boolean = await cli.prompt(
+      'This will remove all saved data for this account including login information and all advanced settings. This action is inreversible, do you want to proceed?'
+    );
+    if (!confirmed) {
+      this.exit(1);
+    }
+    this.configHandler.removeAccount(flags.username!);
+    unlinkSync(this.configHandler.getAuthContextPath(flags.username!));
   }
 }
